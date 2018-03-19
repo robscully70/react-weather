815f57672ae72128a9ffca9d4211da1a3d06e66e
Robert Scully <rtscully@us.ibm.com>
2018-03-18 20:46:41 -0400

    Weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,28 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
-    </div>
+    <h1 className="text-center">About</h1>
+    <p>Important Links</p>
+    <ul>
+      <li>
+        <Link to='https://github.com/robscully70/react-weather'>Weather App</Link>
+      </li>
+      <li>
+      <Link to='https://facebook.github.io/react-native/docs/getting-started.html'>React Get Started</Link>
+      </li>
+      <li>
+      <Link to='https://css-tricks.com/snippets/css/a-guide-to-flexbox/'>Flexbox</Link>
+      </li>
+      <li>
+      <Link to='https://foundation.zurb.com/'>Foundation</Link>
+      </li>
+      <li>
+      <Link to='http://api.openweathermap.org'>Open Weather Map</Link>
+      </li>
+    </ul>
+  </div>
   )
 };
 
